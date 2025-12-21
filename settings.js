1b0c8414b4e9104c5e797b8a4cf0f4abf85f36c2
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-12-21 10:29:35 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,5 @@
 export default {
     ownerNumber: '254703397679',
-    storeWriteInterval: 10000
+    storeWriteInterval: 10000,
+      updateZipUrl: "https://github.com/supreme/??/archive/refs/heads/main.zip"
 };
