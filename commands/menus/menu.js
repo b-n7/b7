1d6eb9ed694c1adf3dc0705a151629d3e2149385
777Wolf-dot <britonkiplangat0@gmail.com>
2026-01-03 02:50:37 +0300

    Update all

--- commands/menus/menu.js
+++ commands/menus/menu.js
@@ -1931,7 +1931,7 @@ case 5: {
   const deploymentPlatform = getDeploymentPlatform();
   
   // Add bot name header before the info section
-  let infoSection = `> *🐺🌕 *${botName}* 🌕🐺*\n`;
+  let infoSection = `> *🐺🌕 ${botName} 🌕🐺*\n`;
   
   // Add info section only if any field is enabled
   const fieldsStatus = getAllFieldsStatus(style);
